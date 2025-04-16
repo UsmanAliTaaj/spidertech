064d3f32892a61f98713c01872f8b0e186843148
UsmanAliTaaj <usman.ali.taaj@gmail.com>
2025-04-16 20:28:16 +0500

    ads remove

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,3 @@
-// import Banner from "@/ui/Banner";
-// import BasicProducts from "@/ui/BasicProducts";
-// import Clients from "@/ui/Clients";
-// import CoreServices from "@/ui/CoreServices";
-// import HighPerformance from "@/ui/HighPerformance";
-// import Projects from "@/ui/Projects";
-// import WhatWeDo from "@/ui/WhatWeDo";
-// import PickServices from "@/ui/services/PickServices";
-
-// export default function Home() {
-//   return (
-//     <main>
-//       <Banner />
-//       <PickServices />
-//       <CoreServices />
-//       <Clients />
-//       <WhatWeDo />
-//       <BasicProducts />
-//       <HighPerformance />
-//       <Projects />
-//     </main>
-//   );
-// }
-
-
-'use client';
-
-import Script from 'next/script';
 import Banner from "@/ui/Banner";
 import BasicProducts from "@/ui/BasicProducts";
 import Clients from "@/ui/Clients";
@@ -37,25 +9,15 @@ import PickServices from "@/ui/services/PickServices";
 
 export default function Home() {
   return (
-    <>
-      {/* ✅ Google AdSense Auto Ads Script */}
-      <Script
-        async
-        strategy="afterInteractive"
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9393520954659937"
-        crossOrigin="anonymous"
-      />
-
-      <main>
-        <Banner />
-        <PickServices />
-        <CoreServices />
-        <Clients />
-        <WhatWeDo />
-        <BasicProducts />
-        <HighPerformance />
-        <Projects />
-      </main>
-    </>
+    <main>
+      <Banner />
+      <PickServices />
+      <CoreServices />
+      <Clients />
+      <WhatWeDo />
+      <BasicProducts />
+      <HighPerformance />
+      <Projects />
+    </main>
   );
 }
